7a7e25720d57fd065c001b1ccf297c72657cd1b7
sl-na <slink0051@gmail.com>
2026-01-19 16:50:28 +0900

    Change variable name from ml to mt

--- recursion_divide_and_conquer/exhaustive_search.py
+++ recursion_divide_and_conquer/exhaustive_search.py
@@ -1,21 +1,21 @@
 from sys import stdin
 
 
-def solve(i, ml, n, at):
-    if ml == 0:
+def solve(i, mt, n, at):
+    if mt == 0:
         return True
     if i >= n:
         return False
-    return solve(i + 1, ml, n, at) or solve(i + 1, ml - at[i], n, at)
+    return solve(i + 1, mt, n, at) or solve(i + 1, mt - at[i], n, at)
 
 
 def main():
     n = int(stdin.readline())
     at = tuple(map(int, stdin.readline().split()))
     q = int(stdin.readline())
-    ml = tuple(map(int, stdin.readline().split()))
+    mt = tuple(map(int, stdin.readline().split()))
     for i in range(q):
-        if solve(0, ml[i], n, at):
+        if solve(0, mt[i], n, at):
             print("yes")
         else:
             print("no")
