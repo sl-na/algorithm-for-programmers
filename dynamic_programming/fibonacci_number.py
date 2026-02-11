265a1d36d7e27ea37275dc886bd72c7728fac3f2
sl-na <slink0051@gmail.com>
2026-02-11 13:16:26 +0900

    Add Fibonacci number calculation using dynamic programming
    - Create new file for Fibonacci DP
    - Initialize fl[0] and fl[1] to 1
    - Compute fib numbers iteratively
    - Print nth Fibonacci number

---
+++ dynamic_programming/fibonacci_number.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input())
+    fl = [None] * (n + 1)
+    fl[0] = fl[1] = 1
+    for i in range(2, n + 1):
+        fl[i] = fl[i - 1] + fl[i - 2]
+    print(fl[n])
+
+
+if __name__ == "__main__":
+    main()
